f6f41abc0a739e5785aa06872087765a666350ad
fmung2 <fmungofa@gmail.com>
2018-06-30 12:53:33 -0700

    new file

---
+++ Age.py
@@ -0,0 +1,12 @@
+print('Hello User')
+
+Name = input("What is your name? ")
+
+print("Hello " + Name)
+
+Age = input("What is your age?")
+
+If int(age)< 30
+   print("Aww you are just a baby")
+If int(age )> 30
+   print("Ah... A well traveled soul are ye.")
