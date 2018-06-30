a4f33fd84e1e489a069b034f9421d330ae058019
Shang <shangwang@outlook.com>
2018-06-30 13:56:51 -0700

    wine

---
+++ Wine.py
@@ -0,0 +1,36 @@
+Group Name: Drunken Masters
+
+Project Title:  Where is the best wine found in the world?
+
+Team Members:  Shang, Stephen, Danny and Farai
+
+Project Description/Outline: Regional, Price, Ingredient, Review Score, Weather, Sentiment analysis of different wines across the world
+
+Research Questions to Answer: 
+What’s the top influential factors on wine prices? (Including Region? Weather? Sentiment? Customer Reviews?
+What’s the correlation between review point and wine tester twitter account popularity/ account sentiment? 
+
+
+
+Data Sets to be Used
+From Kaggle: Wine Reviews by zackthoutt / OpenWeatherMap / Twitter 
+Rough Breakdown of Tasks:
+Farai: The effects of weather on the review points and price of wine?
+ The sentiment analysis of  wine tasters twitter accounts?
+Shang: Clean up data and Where is the most expensive wine found in the world?
+Most reviewed wines and sentiment by region?
+Stephen: Type of grapes that get the most reviews and the price analysis of their end product.
+
+
+
+Where is the most expensive wine found in the world?
+The effects of weather on the review points and price of wine?
+The sentiment analysis of  wine tasters twitter accounts?
+Most reviewed wines and sentiment by region?
+Type of grapes that get the most reviews and the price analysis of their end product.
+Review analysis of winery, average price per bottle.
+
+
+
+
+
