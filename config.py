0c90b66ee3994d9aa2b1d3354370f3a30da8ad17
zjgcainiao <zjucainiao@yahoo.com>
2018-07-16 01:34:40 -0700

    data visulization Stephen - Geographic

---
+++ config.py
@@ -0,0 +1,5 @@
+# Enter your API keys
+gkey = "AIzaSyCjesixQLX2YTAV-5MxxL5uPJ_IRihHIS0"
+#"AIzaSyDqyiML69kxTbKolq63B1GhFaP_VL5CEP0"
+
+census_key ="85ac64b6b5a9c0901b00329d1ef41f0c53ccfc98"
---
+++ test.py
@@ -0,0 +1,3 @@
+import geoplotlib
+
+print("Hello Stephen")
